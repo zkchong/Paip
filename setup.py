593016d70711b467d57dd2e5f784adefb2d307b0
Zan Kai <zankai@axiatadigital.com>
2020-06-28 10:30:20 +0800

    setup.py for pypi.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="paip-zkchong", # Replace with your own username
+    version="0.0.1.dev",
+    author="Zan-Kai Chong",
+    author_email="zkchong@gmail.com",
+    description="Simple pipeline alternative to Scikit Learn's Pipeline.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zkchong/Paip",
+    packages=setuptools.find_packages(),
+    install_requires = ['pandas>=0.25'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    python_requires='>=3.0',
+)
